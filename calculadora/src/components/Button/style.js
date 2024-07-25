c797bfb64fc4cf4a1a12a34b0c260453abd21915
Ruan Pablo <rp824194@gmail.com>
2024-07-25 11:41:32 -0300

    Adição e personalização dos botões

---
+++ calculadora/src/components/Button/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #0BAAF0;
+    color: #FFF;
+    font-size: 24px;
+    font-weight:700;
+    flex: 1;
+
+`
